29e37520bb6a33faf1c4545b8dd56787eaffb877
Orange-Tofu <108827497+Orange-Tofu@users.noreply.github.com>
2022-12-18 22:53:33 +0530

    Update settings.py

--- learn/settings.py
+++ learn/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&njzmgt&nrhdm)u40=nnwayt5yzfzip7#=8+#ry4$+i(7htvz2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
