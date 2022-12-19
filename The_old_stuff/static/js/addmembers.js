0813f3575a0a05284754298881bd739ed548cf02
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-12-19 22:25:35 +0530

    Update addmembers.js

--- The_old_stuff/static/js/addmembers.js
+++ The_old_stuff/static/js/addmembers.js
@@ -4,23 +4,28 @@ const Peoples={"Bhawesh Agarwal": ["FrontEnd","https://github.com/Bhawesh02","ht
 const CardContainer = document.getElementById("CardContainer");  
 //For Every person insert card 
 Object.keys(Peoples).forEach(People => {
-    //console.log(Peoples[People][1]);
+    //Creted div with class div;
     var div = document.createElement('div');
     div.classList.add("card");
     CardContainer.appendChild(div);
+    //Creted div with class content and make card as parent;
     var div1 = document.createElement('div');
     div1.classList.add("content");
     div.appendChild(div1);
+     //Creted div with class imgbx and make content as parent;
     var div2 = document.createElement('div');
     div2.classList.add("imgBx");
     div1.appendChild(div2);
+    //create img with imgbx as parent
     var img = document.createElement('img');
     img.setAttribute("src",Peoples[People][3]);
     div2.appendChild(img);
+    //Creted div with class contentBx and make content as parent
     var div4 = document.createElement('div');
     div4.classList.add("contentBx");
     div4.innerHTML="<h3>"+People+"<br /><span>"+Peoples[People][0]+"</span></h3>";
     div1.appendChild(div4);
+    //added git and likdin to card as parent
     var ul = document.createElement('ul');
     ul.classList.add("sci");
     div.appendChild(ul);
