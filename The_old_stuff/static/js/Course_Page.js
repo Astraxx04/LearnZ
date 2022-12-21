3d18187c61911c9d301179d09b4a8a20b845d99d
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-12-21 14:21:43 +0530

    Update Course_Page.js

--- The_old_stuff/static/js/Course_Page.js
+++ The_old_stuff/static/js/Course_Page.js
@@ -1,8 +1,12 @@
 
 //Course name - Key,, current chapter - 0 index,, chapter name - 1 index
 var Courses={"JavaScript Fundamentals": ["Chapter 4","Callbacks & Closures"],"Data Science": ["Chapter 4","Callbacks & Closures"],"C programming": ["Chapter 4","Callbacks & Closures"],"Data Communication": ["Chapter 4","Callbacks & Closures"],"Data structure Fundamentals": ["Chapter 4","Callbacks & Closures"],"Micro Processor": ["Chapter 4","Callbacks & Closures"]};
+
 var NoOfSemester = 6;
+
 var user_usn = "1DS20IS027";
+
+//const from html
 const CourseContainer = document.getElementById("CourseContainer");  
 const dropdown = document.getElementById("dropdown");  
 const useinfo = document.getElementById("useinfo");  
