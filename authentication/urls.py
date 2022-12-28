cc21c0a07691023651dd6ff6901b8b510e238f04
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-12-28 19:04:17 +0530

    some changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('course', views.course, name='course'),
     path('student_signup', views.student_signup, name='student_signup'),
     path('student_signin', views.student_signin, name='student_signin'),
+    path('teacher_signup', views.teacher_signup, name='teacher_signup'),
+    path('teacher_signin', views.teacher_signin, name='teacher_signin'),
     # path('activate/<uidb64>/<token>', views.activate, name='activate'),
     # path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
