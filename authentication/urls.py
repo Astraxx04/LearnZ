fe23da3342b8d15718b996bb6c1fc59c7ba7f675
Aditi <aditipandeya1608@gmail.com>
2022-12-28 18:49:33 +0530

    teacher page

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('student_signup', views.student_signup, name='student_signup'),
     path('student_signin', views.student_signin, name='student_signin'),
+    path('teacher_signup', views.teacher_signup, name='teacher_signup'),
     # path('activate/<uidb64>/<token>', views.activate, name='activate'),
     # path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
