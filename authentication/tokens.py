21b5b8346338d94e79865abdc82195b4c5aea833
Orange-Tofu <clumsymonkey@tutanota.com>
2022-12-28 15:36:12 +0530

    Minor change

--- authentication/tokens.py
+++ authentication/tokens.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
-from six import text_type
+# from six import text_type
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self,user,timestamp):
