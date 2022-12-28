a17e81aad57ebe6a068f53a9c23d5af249760b42
Orange-Tofu <clumsymonkey@tutanota.com>
2022-12-29 01:25:00 +0530

    Created py file to access db

---
+++ try.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+sqliteConnection = sqlite3.connect('db.sqlite3')
+cursor = sqliteConnection.cursor()
+
+print('DB Init')
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# print(cursor.fetchall())
+
+# cursor.execute("SELECT * FROM auth_user;")
+# for i in cursor.fetchall():
+#     print(i)
+# print(cursor.fetchall())
+
+# print('\n\n')
+cursor.execute("SELECT sql FROM sqlite_master WHERE name='auth_user';")
+for i in cursor.fetchall():
+    print()
+    print(i)
+
+cursor.close()
+sqliteConnection.close()
