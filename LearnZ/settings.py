16cf331ac9a8503b1e482ecfbf59627fdfdbf74c
Orange-Tofu <clumsymonkey@tutanota.com>
2022-12-28 15:41:03 +0530

    Changed allowed host

--- LearnZ/settings.py
+++ LearnZ/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-wdke-rd%j16gdotni4rj$mgdqy__%d4#sin44zug-z67e!(xg0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
