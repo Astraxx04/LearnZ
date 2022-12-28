21af998baf3d37b3b3a37d040bc9a91fb86874ea
Orange-Tofu <clumsymonkey@tutanota.com>
2022-12-29 00:03:28 +0530

    removed py auto gui

--- authentication/views.py
+++ authentication/views.py
@@ -12,7 +12,7 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 import django
 from django.utils.encoding import force_str
-import pyautogui
+# import pyautogui
 django.utils.encoding.force_text = force_str
 
 
@@ -35,23 +35,23 @@ def student_signup(request):
         pass2 = request.POST['ConfiPass']
 
         if User.objects.filter(username=username):
-            pyautogui.alert("USN already exist! Please try some other username.")
+            # pyautogui.alert("USN already exist! Please try some other username.")
             return redirect('index')
 
         if User.objects.filter(email=email).exists():
-            pyautogui.alert("Email Already Registered!!")
+            # pyautogui.alert("Email Already Registered!!")
             return redirect('index')
 
         if len(username) > 20:
-            pyautogui.alert("Usn must be under 20 charcters!!")
+            # pyautogui.alert("Usn must be under 20 charcters!!")
             return redirect('index')
 
         if pass1 != pass2:
-            pyautogui.alert("Passwords didn't matched!!")
+            # pyautogui.alert("Passwords didn't matched!!")
             return redirect('index')
 
         if not username.isalnum():
-            pyautogui.alert("Username must be Alpha-Numeric!!")
+            # pyautogui.alert("Username must be Alpha-Numeric!!")
             return redirect('index')
 
         myuser = User.objects.create_user(username, email, pass1)
@@ -61,7 +61,7 @@ def student_signup(request):
         # myuser.is_active = False
         #myuser.is_active = False
         myuser.save()
-        pyautogui.alert("Your Account has been created succesfully!!.")
+        # pyautogui.alert("Your Account has been created succesfully!!.")
 
 
         return redirect('student_signin')
@@ -80,23 +80,23 @@ def teacher_signup(request):
         pass2 = request.POST['ConfiPass']
 
         if User.objects.filter(username=username):
-            pyautogui.alert("teacherid already exist! Please try some other username.")
+            # pyautogui.alert("teacherid already exist! Please try some other username.")
             return redirect('index')
 
         if User.objects.filter(email=email).exists():
-            pyautogui.alert("Email Already Registered!!")
+            # pyautogui.alert("Email Already Registered!!")
             return redirect('index')
 
         if len(username) > 20:
-            pyautogui.alert("Usn must be under 20 charcters!!")
+            # pyautogui.alert("Usn must be under 20 charcters!!")
             return redirect('index')
 
         if pass1 != pass2:
-            pyautogui.alert("Passwords didn't matched!!")
+            # pyautogui.alert("Passwords didn't matched!!")
             return redirect('index')
 
         if not username.isalnum():
-            pyautogui.alert("Username must be Alpha-Numeric!!")
+            # pyautogui.alert("Username must be Alpha-Numeric!!")
             return redirect('index')
 
         myuser = User.objects.create_user(username, email, pass1)
@@ -106,7 +106,7 @@ def teacher_signup(request):
         # myuser.is_active = False
         #myuser.is_active = False
         myuser.save()
-        pyautogui.alert("Your Account has been created succesfully!!.")
+        # pyautogui.alert("Your Account has been created succesfully!!.")
 
 
         return redirect('teacher_signin')
@@ -123,7 +123,7 @@ def student_signin(request):
         if user is not None:
             login(request, user)
             fname = user.first_name
-            pyautogui.alert("Logged In Sucessfully!!")
+            # pyautogui.alert("Logged In Sucessfully!!")
             return render(request, "courses/Course_Page.html",{"fname":fname})
         else:
             return redirect('index')
@@ -158,7 +158,7 @@ def activate(request, uidb64, token):
         # user.profile.signup_confirmation = True
         myuser.save()
         login(request, myuser)
-        pyautogui.alert("Your Account has been activated!!")
+        # pyautogui.alert("Your Account has been activated!!")
         return redirect('student_signin')
     else:
         return render(request, 'activation_failed.html')
@@ -166,5 +166,5 @@ def activate(request, uidb64, token):
 
 def signout(request):
     logout(request)
-    pyautogui.alert("Logged Out Successfully!!")
+    # pyautogui.alert("Logged Out Successfully!!")
     return redirect('index')
