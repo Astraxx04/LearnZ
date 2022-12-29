0e2c1b5ef67dd3e02f8d3d0a7b40731a8841b6cf
Astraxx04 <gagan200254@gmail.com>
2022-12-29 12:35:30 +0530

    gitignore update

--- courses/static/courses/js/Course_Page.js
+++ courses/static/courses/js/Course_Page.js
@@ -42,3 +42,12 @@ Object.keys(Courses).forEach(Course => {
   });
 
   //edited
+
+
+
+  let db = new sqlite3.Database('../db.sqlite3', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+  });
