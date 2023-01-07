16a3d011b0a56459bdf6aec4a4bf79984b0ffd91
Ayushi <fierypitsofhello@gamil.com>
2023-01-07 21:02:57 +0530

    continue for quiz and syllabus

--- features/urls.py
+++ features/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('student', views.studentFeatures, name='feature_page_student'),
     path('teacher', views.teacherFeatures, name='feature_page_teacher'),
     path('question_bank', views.questionBank, name='qbDownload'),
+    path('quiz', views.Quiz, name='quizDownload'),
+    path('syllabus', views.Syllabus, name='sybDownload'),
 ]
--- features/views.py
+++ features/views.py
@@ -30,3 +30,45 @@ def questionBank(request):
     return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
     return redirect('feature_page_student')
 
+def Quiz(request):
+    sqliteConnection = sqlite3.connect('db.sqlite3')
+    cursor = sqliteConnection.cursor()
+    print('DB Init')
+
+    if 'courseStudent' in request.session:
+        name = request.session['courseStudent']
+
+        query = f"SELECT crs_id FROM course_course WHERE course='{name}' "
+        location = cursor.execute(query)
+        cursor.close()
+
+        filepath = r'features\tt.pdf'
+        return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+        # filename = employee + ".pdf"
+        # filepath = os.path.join(settings.MEDIA_ROOT, "payslips", year, month, filename)
+
+    filepath = r'features\tt.pdf'
+    return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+    return redirect('feature_page_student')
+
+def Syllabus(request):
+    sqliteConnection = sqlite3.connect('db.sqlite3')
+    cursor = sqliteConnection.cursor()
+    print('DB Init')
+
+    if 'courseStudent' in request.session:
+        name = request.session['courseStudent']
+
+        query = f"SELECT crs_id FROM course_course WHERE course='{name}' "
+        location = cursor.execute(query)
+        cursor.close()
+
+        filepath = r'features\tt.pdf'
+        return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+        # filename = employee + ".pdf"
+        # filepath = os.path.join(settings.MEDIA_ROOT, "payslips", year, month, filename)
+
+    filepath = r'features\tt.pdf'
+    return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+    return redirect('feature_page_student')
+
