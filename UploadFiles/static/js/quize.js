201f3650172164e042574cc3cfeddb5937df1b99
Bhawesh02 <bhaweshagarwal02@gmail.com>
2023-01-08 14:22:35 +0530

    api disabled

--- UploadFiles/static/js/quize.js
+++
@@ -1,4 +0,0 @@
-var current_course = getCookie("course_name");
-var set_cour = document.getElementById("id_quiz_course_name");
-set_cour.setAttribute("value",current_course);
-console.log(current_course);
