7cfa174f0498a2f254ec95733d9124e91cac28e7
Astraxx04 <gagan200254@gmail.com>
2023-01-08 07:37:40 +0530

    Rake nltk model

--- UploadFiles/views.py
+++ UploadFiles/views.py
@@ -4,6 +4,13 @@ from .models import notes,sylabus
 from django.contrib import messages
 from django.urls import path
 import os
+import PyPDF2
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from rake_nltk import Rake
+import re
+import json
 
 # Create your views here.
 def notesbase(request):
@@ -63,6 +70,7 @@ def syluploadfile(request):
             else:
                 sylabus.objects.create(file_name=MyFileName,my_file=MyFile).save()
                 messages.success(request,"File uploaded successfully.")
+                suggestfun()
         return redirect("sylbase")
 
 def syldeleteFile(request,id):
@@ -71,3 +79,45 @@ def syldeleteFile(request,id):
     os.remove(mydata.my_file.path)
     messages.success(request,'File deleted successfully.')  
     return redirect('sylbase')
+
+
+def suggestfun():
+
+    pdffileobj=open('upload/Testing.pdf','rb')
+    pdfreader=PyPDF2.PdfReader(pdffileobj)
+    x=len(pdfreader.pages)
+    pageobj=pdfreader.pages[0]
+    text=pageobj.extract_text()
+    print(text)
+    r=Rake ()
+    r.extract_keywords_from_text(text)
+    keyword_extracted = r.get_ranked_phrases()[2:25]
+    print(keyword_extracted)
+    x=keyword_extracted
+    print(x)
+    def unwanted_number(x):
+        for k in x:
+            numbers = re.findall('[0-9]+', k)
+            if(numbers):
+                x.remove(k)
+        print(x)
+
+    def unwanted_text(x):
+        for k in x:
+            k = re.sub("\d+", "",k)
+            text = re.findall('(.*?)dayananda(.*?)', k) or re.findall('(.*?)university(.*?)', k) or re.findall('(.*?)malleshwara(.*?)', k) or re.findall('(.*?)institute(.*?)', k) or re.findall('(.*?)engineering(.*?)', k)
+            if(text):
+                x.remove(k)
+        print(x)
+
+
+    print(keyword_extracted)
+    unwanted_number(x)
+    print("\n\n\n")
+    unwanted_text(x)
+
+
+    jsonString = json.dumps(x)
+    jsonFile = open("data.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
