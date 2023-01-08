e7f663d078693ad34f4b9a89428ce4142368e1f3
Astraxx04 <gagan200254@gmail.com>
2023-01-08 16:33:01 +0530

    change

--- features/urls.py
+++ features/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('student', views.studentFeatures, name='feature_page_student'),
     path('teacher', views.teacherFeatures, name='feature_page_teacher'),
-    path('notes', views.Notes, name='notesDownload'),
-    path('quiz', views.Quiz, name='quizDownload'),
-    path('syllabus', views.Syllabus, name='sybDownload'),
+    path('notes', views.notes, name='notesDownload'),
+    # path('quiz', views.quiz, name='quizDownload'),
+    # path('syllabus', views.syllabus, name='sybDownload'),
     path('suggestedvideos', views.suggestedvideos, name='suggestedvideos'),
 ]
