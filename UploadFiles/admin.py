839d5386062c018546ef07f5a240b0c2fa43ff92
Janesh7 <janeshwalia273@gmail.com>
2023-01-08 22:54:29 +0530

    added admin sylabus and quiz

--- UploadFiles/admin.py
+++ UploadFiles/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import notes
+from .models import notes,sylabus,quiz
 
 # Register your models here.
 
 admin.site.register(notes)
+admin.site.register(sylabus)
+admin.site.register(quiz)
